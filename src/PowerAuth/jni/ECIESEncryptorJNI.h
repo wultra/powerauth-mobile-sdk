8278209edbdcababb4c14d192ea9940f42a6ee8d
Juraj Ďurech <durech.juraj@gmail.com>
2017-11-24 15:56:06 +0100

    Android: Added JNI wrapper for ECIES related classes.

---
+++ src/PowerAuth/jni/ECIESEncryptorJNI.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2017 Lime - HighTech Solutions s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <cc7/jni/JniHelper.h>
+#include <PowerAuth/ECIES.h>
+
+/**
+ Creates a new ECIESCryptogram java object from given C++ cryptogram structure.
+ */
+CC7_EXTERN_C jobject CreateJavaCryptogramFromCppObject(JNIEnv * env, io::getlime::powerAuth::ECIESCryptogram & cryptogram);
+
+/**
+ Loads a content from ECIESCryptogram java object into cryptogram C++ structure.
+ */
+CC7_EXTERN_C void LoadCppCryptogramFromJavaObject(JNIEnv * env, jobject cryptogram, io::getlime::powerAuth::ECIESCryptogram & cppCryptogram);

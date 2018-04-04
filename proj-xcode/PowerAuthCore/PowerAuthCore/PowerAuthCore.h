e7772223155fd948d8e24a7a2ee8b1d9cfe53343
Juraj Ďurech <durech.juraj@gmail.com>
2018-04-04 18:47:40 +0200

    IOS: Added PowerAuthCore static library containing low level C++ codes & thin ObjC wrapper.
    PowerAuthLib project now provides only high level SDK codes and depends on PowerAuthCore

---
+++ proj-xcode/PowerAuthCore/PowerAuthCore/PowerAuthCore.h
@@ -0,0 +1,13 @@
+//
+//  PowerAuthCore.h
+//  PowerAuthCore
+//
+//  Created by Juraj Durech on 03/04/2018.
+//  Copyright © 2018 Lime - HighTech Solutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PowerAuthCore : NSObject
+
+@end

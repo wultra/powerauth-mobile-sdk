c52bff83c584a4488fa54dd92bd137be6e2e46c1
Juraj Ďurech <durech.juraj@gmail.com>
2018-12-03 15:40:59 +0100

    IOS: Deprecated methods are now marked for upcoming version 0.20.0

--- proj-xcode/Classes/sdk/PowerAuthSDK.h
+++ proj-xcode/Classes/sdk/PowerAuthSDK.h
@@ -171,7 +171,7 @@
 									   customAttributes:(nullable NSDictionary<NSString*,NSString*>*)customAttributes
 													url:(nonnull NSURL*)url
 											httpHeaders:(nullable NSDictionary*)httpHeaders
-											   callback:(nonnull void(^)(PA2ActivationResult * _Nullable result, NSError * _Nullable error))callback PA2_DEPRECATED(0.19.4);
+											   callback:(nonnull void(^)(PA2ActivationResult * _Nullable result, NSError * _Nullable error))callback PA2_DEPRECATED(0.20.0);
 
 /** Create a new activation with given name and custom activation data by calling a custom RESTful API endpoint.
  
@@ -185,7 +185,7 @@
 - (nullable PA2OperationTask*) createActivationWithName:(nullable NSString*)name
 									 identityAttributes:(nonnull NSDictionary<NSString*,NSString*>*)identityAttributes
 													url:(nonnull NSURL*)url
-											   callback:(nonnull void(^)(PA2ActivationResult * _Nullable result, NSError * _Nullable error))callback PA2_DEPRECATED(0.19.4);
+											   callback:(nonnull void(^)(PA2ActivationResult * _Nullable result, NSError * _Nullable error))callback PA2_DEPRECATED(0.20.0);
 
 /** Commit activation that was created and store related data using provided authentication instance.
  
@@ -314,7 +314,7 @@
 																	 method:(nonnull NSString*)method
 																	  uriId:(nonnull NSString*)uriId
 																	   body:(nullable NSData*)body
-																	  error:(NSError * _Nullable * _Nullable)error PA2_DEPRECATED(0.19.4);
+																	  error:(NSError * _Nullable * _Nullable)error PA2_DEPRECATED(0.20.0);
 
 /** Compute the offline signature for given HTTP method, URI identifier and HTTP request body using provided authentication information.
  

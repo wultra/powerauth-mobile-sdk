2294d943ad8f40d74447b647a59a39bcd9d4cacd
Juraj Ďurech <1719814+hvge@users.noreply.github.com>
2018-12-06 15:48:08 +0100

    Fixed #130: Added support for request interceptors

---
+++ proj-android/PowerAuthLibrary/src/main/java/io/getlime/security/powerauth/networking/interceptors/CustomHeaderRequestInterceptor.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2018 Wultra s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.getlime.security.powerauth.networking.interceptors;
+
+import android.support.annotation.NonNull;
+
+import java.net.HttpURLConnection;
+
+/**
+ * The {@code CustomHeaderRequestInterceptor} class implements {@link HttpRequestInterceptor} interface
+ * and allows you set an arbitrary HTTP header to requests, executed internally in PowerAuth SDK.
+ */
+public class CustomHeaderRequestInterceptor implements HttpRequestInterceptor {
+
+    private final String headerKey;
+    private final String headerValue;
+
+    /**
+     * @param headerKey String with HTTP header key.
+     * @param headerValue String with corresponding header's value.
+     */
+    public CustomHeaderRequestInterceptor(@NonNull String headerKey, @NonNull String headerValue) {
+        this.headerKey = headerKey;
+        this.headerValue = headerValue;
+    }
+
+    /**
+     * @return HTTP header's key provided in object initialization.
+     */
+    public @NonNull String getHeaderKey() {
+        return headerKey;
+    }
+
+    /**
+     * @return HTTP header's value provided in object initialization.
+     */
+    public @NonNull String getHeaderValue() {
+        return headerValue;
+    }
+
+    @Override
+    public void processRequestConnection(@NonNull HttpURLConnection connection) {
+        connection.addRequestProperty(headerKey, headerValue);
+    }
+}
---
+++ proj-xcode/TestClasses/PA2CustomHeaderRequestInterceptorTests.m
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2018 Wultra s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import <XCTest/XCTest.h>
+#import <PowerAuth2/PowerAuth2.h>
+
+@interface PA2CustomHeaderRequestInterceptorTests : XCTestCase
+@end
+
+@implementation PA2CustomHeaderRequestInterceptorTests
+
+- (void) testCustomHeaderRequestInterceptor
+{
+	PA2CustomHeaderRequestInterceptor * interceptor = [[PA2CustomHeaderRequestInterceptor alloc] initWithHeaderKey:@"X-Custom" value:@"CustomHeaderValue"];
+	XCTAssertEqualObjects(interceptor.headerKey, @"X-Custom");
+	XCTAssertEqualObjects(interceptor.headerValue, @"CustomHeaderValue");
+	
+	NSMutableURLRequest * request = [[NSMutableURLRequest alloc] init];
+	[interceptor processRequest: request];
+	XCTAssertTrue([request.allHTTPHeaderFields[@"X-Custom"] isEqualToString:@"CustomHeaderValue"]);
+}
+
+@end

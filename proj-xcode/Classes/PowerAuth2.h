4887cd8a8dd36030499427f5a5ca572f9de5aeec
Juraj Ďurech <durech.juraj@gmail.com>
2020-05-27 16:21:50 +0200

    Core: Fix #284: Added support for tvOS Core: Fix #275: Added support for macOSX Catalyst Core: Fix #298: Added bitcode to all Apple platforms cc7 updated to v0.2.1 (contains OpenSSL 1.1.1g)

--- proj-xcode/Classes/PowerAuth2.h
+++ proj-xcode/Classes/PowerAuth2.h
@@ -1,5 +1,5 @@
 /**
- * Copyright 2018 Wultra s.r.o.
+ * Copyright 2018-2020 Wultra s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for PowerAuth2.
 FOUNDATION_EXPORT double PowerAuth2VersionNumber;

afb1151ec159856a3a7fdc3b091f9782f7b9530a
Jan Pešek <jan.pesek@wultra.com>
2025-10-06 13:36:05 +0200

    Fix #750: Modify Request class to allow protocol upgrade scenario (#751)

--- src/PowerAuth/request/EndpointSpec.h
+++ src/PowerAuth/request/EndpointSpec.h
@@ -28,7 +28,8 @@ struct EndpointSpec
         FL_ALLOWED_IN_UPGRADE       = 1 << 1,
         FL_SYNCHRONIZE_TIME         = 1 << 2,
         FL_NOT_WRAPPED              = 1 << 3,
-        FL_PENDING_REGISTRATION     = 1 << 4
+        FL_PENDING_REGISTRATION     = 1 << 4,
+        FL_FORCE_ENCRYPTION_HEADER  = 1 << 5
     };
     
     ProtocolVersion version;
@@ -72,6 +73,11 @@ struct EndpointSpec
     {
         return !isEncrypted() && (flags & FL_NOT_WRAPPED) == 0;
     }
+    
+    bool forceEncryptionHeader() const noexcept
+    {
+        return (flags & FL_FORCE_ENCRYPTION_HEADER) == FL_FORCE_ENCRYPTION_HEADER;
+    }
 };
 
 namespace v4 {
@@ -89,6 +95,8 @@ extern const EndpointSpec Endpoint_VaultUnlock;
 extern const EndpointSpec Endpoint_TokenCreate;
 extern const EndpointSpec Endpoint_TokenRemove;
 extern const EndpointSpec Endpoint_ValidateCredentials;
+extern const EndpointSpec Endpoint_ProtocolUpgradeStart;
+extern const EndpointSpec Endpoint_ProtocolUpgradeConfirm;
 
 } // namespace v4
 

277ba62d0f4db27a88fe495e99ba6d2f0718036f
Jan Pešek <jan.pesek@wultra.com>
2025-10-23 15:43:36 +0200

    Add method docs

--- proj-xcode/PowerAuthCore/PowerAuthCoreSession.h
+++ proj-xcode/PowerAuthCore/PowerAuthCoreSession.h
@@ -145,8 +145,14 @@
  This property access the session's state, so read access must be guaranteed.
  */
 @property (nonatomic, assign, readonly) PowerAuthCoreProtocolVersion protocolVersion;
-
-@property (nonatomic, assign, readonly, nonnull) NSDictionary<NSString*, NSObject*>* lastUserInfo;
+/**
+ Contains last user info received from the server during activation or explicit user info fetch.
+ This property provides the most recent user info and does not trigger any server communication.
+ If no such information has been received yet, nil is returned.
+ 
+ This property access the session's state, so read access must be guaranteed.
+ */
+@property (nonatomic, assign, readonly, nullable) NSDictionary<NSString*, NSObject*>* lastUserInfo;
 
 
 #pragma mark - Serialization
@@ -239,6 +245,13 @@
 
 #pragma mark - User info
 
+/// Fetch user info from the server.
+///
+/// This function caches the received user info in the session, so write access must be guaranteed.
+///
+/// - Parameters:
+///   - error: Pointer where error is stored in case of failure.
+/// - Returns: Core request object containing all required information for user info fetch.
 - (nullable PowerAuthCoreRequest*) fetchUserInfo:(NSError*_Nullable*_Nullable)error;
 
 #pragma mark - Signature keys management

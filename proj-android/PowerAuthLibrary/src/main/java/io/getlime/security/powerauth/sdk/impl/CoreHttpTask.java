82caa84a88b636e20537295953b8307bb457c5b9
Juraj Ďurech <durech.juraj@gmail.com>
2026-01-23 14:48:43 +0100

    Android: Added missing documentation to CoreHttpClient and CoreHttpTask

--- proj-android/PowerAuthLibrary/src/main/java/io/getlime/security/powerauth/sdk/impl/CoreHttpTask.java
+++ proj-android/PowerAuthLibrary/src/main/java/io/getlime/security/powerauth/sdk/impl/CoreHttpTask.java
@@ -26,11 +26,23 @@ import io.getlime.security.powerauth.exception.PowerAuthErrorCodes;
 import io.getlime.security.powerauth.exception.PowerAuthErrorException;
 import io.getlime.security.powerauth.networking.interfaces.INetworkResponseListener;
 
+/**
+ * The {@code CoreHttpTask} wraps {@link CoreTask} and executes series of requests specified in
+ * the task.
+ * @param <TResponse> Type of response. Use {@link Object} if there's no response.
+ */
 public class CoreHttpTask<TResponse> extends CompositeCancelableTask {
 
     private final CoreTask<TResponse> task;
     private final CoreHttpClient httpClient;
     private final INetworkResponseListener<TResponse> listener;
+
+    /**
+     * Construct task with required parameters.
+     * @param task {@link CoreTask} containing sequence of requests to execute.
+     * @param httpClient Parent {@link CoreHttpClient}.
+     * @param listener Callback listener interface.
+     */
     public CoreHttpTask(@NonNull CoreTask<TResponse> task,
                         @NonNull CoreHttpClient httpClient,
                         @NonNull INetworkResponseListener<TResponse> listener) {
@@ -40,10 +52,17 @@ public class CoreHttpTask<TResponse> extends CompositeCancelableTask {
         this.listener = listener;
     }
 
+    /**
+     * Start the task.
+     */
     public void start() {
         processNext();
     }
 
+    /**
+     * Process the next request, if there's any. If no next request is specified, then set
+     * the whole task as complete.
+     */
     private void processNext() {
         try {
             final CoreRequest<Object> request = task.getNextRequest();
@@ -67,7 +86,9 @@ public class CoreHttpTask<TResponse> extends CompositeCancelableTask {
 
                     @Override
                     public void onCancel() {
-
+                        if (setCompleted()) {
+                            listener.onCancel();
+                        }
                     }
                 }));
             }
@@ -76,12 +97,19 @@ public class CoreHttpTask<TResponse> extends CompositeCancelableTask {
         }
     }
 
+    /**
+     * Set the task finished with an error.
+     * @param throwable Error to set as the result of the operation.
+     */
     private void setFinished(@NonNull Throwable throwable) {
         if (setCompleted()) {
             listener.onNetworkError(throwable);
         }
     }
 
+    /**
+     * Set the task finished with the success.
+     */
     private void setFinished() {
         if (setCompleted()) {
             try {

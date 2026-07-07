8657244ccdebaa7f595fb83d1ecdf43cfaff85ac
Jan Kobersky <kober32@gmail.com>
2026-07-07 15:41:41 +0200

    Improvements

--- proj-xcode/PowerAuth2/PowerAuthSDK.m
+++ proj-xcode/PowerAuth2/PowerAuthSDK.m
@@ -1460,8 +1460,10 @@ static PowerAuthSDK * s_inst;
         return nil;
     }
     return [_client postCoreRequest:request completion:^(PowerAuthCoreRequest * request, id response, NSError * error) {
-        // Store the biometry KEK into the keychain only after the server confirms the operation
-        if (!error) {
+        // Store the biometry KEK into the keychain only after the server confirms the operation.
+        if (!error && biometryKekData) {
+            // remove biometry key and store new one
+            [_biometryOnlyKeychain deleteDataForKey:_biometryKeyIdentifier];
             [_biometryOnlyKeychain setSecureData:biometryKekData
                                           forKey:_biometryKeyIdentifier
                                           access:_biometricConfiguration.biometricItemAccess];

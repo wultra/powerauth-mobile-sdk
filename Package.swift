5334d82206b67e2b9103625862e392eebaddfec7
Jan Pešek <jan.pesek@wultra.com>
2026-08-05 02:52:44 -0400

    Fix #929: Update CC7 dependency to 0.7.2 (#930)

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "PowerAuth2", type: .dynamic, targets: ["PowerAuth2"])
     ],
     dependencies: [
-        .package(url: "https://github.com/wultra/cc7", from: "0.7.0")
+        .package(url: "https://github.com/wultra/cc7", from: "0.7.2")
     ],
     targets: [
         
